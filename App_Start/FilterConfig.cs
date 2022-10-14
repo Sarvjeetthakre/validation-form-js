1dd518a1dd3446b3d84170de41969c165d36d54a
sarvjeet14 <sarvjeet14@outlook.com>
2022-10-14 20:00:37 +0530

    js form made by sarvjeet

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Validation_form_javascript
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Validation_form_javascript.MvcApplication" Language="C#" %>
